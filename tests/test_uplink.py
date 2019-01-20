93ef4b4f817fee62fffaccad002ffdd2fa501095
Daniel Palmer <daniel@0x0f.com>
2019-01-20 20:21:14 +0900

    End to end uplink test

--- tests/test_uplink.py
+++ tests/test_uplink.py
@@ -1,7 +1,8 @@
 import pytest
-from tlwpy.tlwbe import Tlwbe, Result
+from tlwpy.tlwbe import Tlwbe, Result, Uplink
 from tlwpy.gatewaysimulator import Gateway
 from tlwpy.pktfwdbr import PacketForwarder
+import asyncio
 
 
 @pytest.mark.asyncio
@@ -18,4 +19,12 @@ async def test_uplink(mosquitto_process, tlwbe_process, dev, tlwbe_client: Tlwbe
     assert 0 <= dev_addr <= 0xffffffff
     assert len(network_key) is 16
     assert len(app_key) is 16
-    await gateway.send_uplink(2, dev_addr, 0, 0, network_key, app_key)
+
+    tlwbe_client.listen_for_uplinks(app_eui, dev_eui, 1)
+    framecounter = 0
+    payload = b'abc123'
+    await gateway.send_uplink(2, dev_addr, framecounter, 1, network_key, app_key, payload=payload)
+    framecounter += 1
+    uplink: Uplink = await asyncio.wait_for(tlwbe_client.queue_uplinks.get(), 10)
+    assert len(uplink.payload) == len(payload)
+    assert uplink.payload == payload
