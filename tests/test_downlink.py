20b6955f41f1fb99a9523c459b563c179d3d5ef1
Daniel Palmer <daniel@0x0f.com>
2019-01-21 22:54:13 +0900

    A bit more end to end testing

--- tests/test_downlink.py
+++ tests/test_downlink.py
@@ -1,13 +1,14 @@
 import pytest
 from tlwpy.tlwbe import Tlwbe, Result
-from tlwpy.gatewaysimulator import Gateway
+from tlwpy.gatewaysimulator import Gateway, Node
 from tlwpy.pktfwdbr import PacketForwarder
 from tlwpy.lorawan import JoinAccept
 import asyncio
 
 
 @pytest.mark.asyncio
-async def test_downlink(mosquitto_process, tlwbe_process, dev, tlwbe_client: Tlwbe, gateway: Gateway):
+async def test_downlink(mosquitto_process, tlwbe_process, dev, tlwbe_client: Tlwbe, gateway: Gateway,
+                        pktfwdbr: PacketForwarder):
     assert mosquitto_process.poll() is None
     assert tlwbe_process.poll() is None
 
@@ -15,9 +16,16 @@ async def test_downlink(mosquitto_process, tlwbe_process, dev, tlwbe_client: Tlw
     dev_eui = dev[1]
     dev_key = dev[2]
 
-    await gateway.join(app_eui, dev_eui, dev_key)
+    # create a node and join
+    node = Node(gateway, app_eui, dev_eui, dev_key)
+    await node.join()
 
+    # schedule a downlink and then check it's scheduled
     result: Result = await tlwbe_client.send_downlink(app_eui, dev_eui, 1, b'123abc', False)
     assert result.code == 0
-
     result: Result = await tlwbe_client.list_downlinks(dev_eui=dev_eui)
+
+    # send an uplink from the device
+    await node.send_uplink(1, payload=b'abc123')
+
+    await asyncio.wait_for(pktfwdbr.downlinks.get(), 10)
--- tests/test_uplink.py
+++ tests/test_uplink.py
@@ -1,8 +1,21 @@
 import pytest
 from tlwpy.tlwbe import Tlwbe, Result, Uplink
-from tlwpy.gatewaysimulator import Gateway
+from tlwpy.gatewaysimulator import Gateway, Node
 from tlwpy.pktfwdbr import PacketForwarder
+from tlwpy.lorawan import Downlink
 import asyncio
+
+
+async def send_uplink(tlwbe_client, pktfwdbr, node, confirmed=False):
+    payload = b'abc123'
+    await node.send_uplink(port=1, confirmed=confirmed, payload=payload)
+    uplink: Uplink = await asyncio.wait_for(tlwbe_client.queue_uplinks.get(), 10)
+    assert len(uplink.payload) == len(payload)
+    assert uplink.payload == payload
+
+    if confirmed:
+        downlink: Downlink = await asyncio.wait_for(pktfwdbr.downlinks.get(), 10)
+        assert downlink.ack
 
 
 @pytest.mark.asyncio
@@ -15,16 +28,15 @@ async def test_uplink(mosquitto_process, tlwbe_process, dev, tlwbe_client: Tlwbe
     dev_eui = dev[1]
     dev_key = dev[2]
 
-    dev_addr, network_key, app_key = await gateway.join(app_eui, dev_eui, dev_key)
-    assert 0 <= dev_addr <= 0xffffffff
-    assert len(network_key) is 16
-    assert len(app_key) is 16
+    # create a node and join
+    node = Node(gateway, app_eui, dev_eui, dev_key)
+    await node.join()
 
+    # start listening for uplinks
     tlwbe_client.listen_for_uplinks(app_eui, dev_eui, 1)
-    framecounter = 0
-    payload = b'abc123'
-    await gateway.send_uplink(2, dev_addr, framecounter, 1, network_key, app_key, payload=payload)
-    framecounter += 1
-    uplink: Uplink = await asyncio.wait_for(tlwbe_client.queue_uplinks.get(), 10)
-    assert len(uplink.payload) == len(payload)
-    assert uplink.payload == payload
+
+    # send an unconfirmed uplink
+    await send_uplink(tlwbe_client, pktfwdbr, node)
+
+    # send a confirmed uplink
+    await send_uplink(tlwbe_client, pktfwdbr, node, confirmed=True)
